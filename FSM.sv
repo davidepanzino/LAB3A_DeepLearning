f73226904d7c8f2437eb485538f315c1679617d4
davidepanzino <144232572+davidepanzino@users.noreply.github.com>
2023-12-06 12:42:46 +0100

    Update FSM.sv

--- FSM.sv
+++ FSM.sv
@@ -1,18 +1,19 @@
-module FSM #(parameter int_part=3, fract_part=5, N=8, M=3)(
+module FSM #(parameter int_part=3, fract_part=5, N=4, M=3)(
     input logic clk,
     input logic rst_n,
     input logic start, //tells to start the computation
     input logic signed [int_part+fract_part-1:0] in_values [N-1:0],
-    input logic signed [int_part+fract_part-1:0] weight [M-1][N-1][N-1], //we have as many matrixes as many layers pair we have 
-    output logic signed [int_part+fract_part-1:0] out_values [N-1:0]
-    //output logic [$clog2(N)-1:0] count_neuron    
+    input logic signed [int_part+fract_part-1:0] weight [M-1:0][N-1:0][N-1:0], //we have as many matrixes as many layers pair we have 
+    input logic signed [int_part+fract_part-1:0] bias,
+    output logic signed [int_part+fract_part-1:0] out_values [N-1:0] 
 );
 
     logic [$clog2(M)-1:0] count_layer, count_layer_next;
-    logic [$clog2(N)-1:0] count_neuron, count_neuron_next; 
-    logic signed [int_part+fract_part-1:0] current_weight [N-1][N-1]; 
+    logic [$clog2(N):0] count_neuron, count_neuron_next; 
+    logic signed [int_part+fract_part-1:0] current_weight [N-1:0]; 
     logic signed [int_part+fract_part-1:0] prev_lay_out [N-1:0];
     logic signed [int_part+fract_part-1:0] curr_lay_out [N-1:0];
+    logic signed [int_part+fract_part-1:0] curr_neuron_out;
     
     enum logic [1:0] {idle = 2'b00, computing = 2'b01, done = 2'b10} current_state, next_state;
     
@@ -20,10 +21,13 @@ module FSM #(parameter int_part=3, fract_part=5, N=8, M=3)(
         if(!rst_n) begin
             current_state <= idle;
             count_layer <= 0;
+            prev_lay_out <= in_values;
         end else begin
             current_state <= next_state;
             count_layer <= count_layer_next;
             count_neuron <= count_neuron_next;
+            if(count_neuron==N-1) //not 0 because count_neuron is updated at the end of the block so it's still N-1
+                prev_lay_out <= curr_lay_out;           
         end
     end
 
@@ -40,17 +44,16 @@ module FSM #(parameter int_part=3, fract_part=5, N=8, M=3)(
             idle : begin
                     count_layer_next = 0;
                     count_neuron_next = 0;
-                    prev_lay_out = in_values;
                    end
             computing : begin
-                            if(count_neuron < N) begin
+                            if(count_neuron < N-1) begin
                                 count_neuron_next = count_neuron + 1;
                                 count_layer_next = count_layer;
                             end else begin
                                 count_neuron_next = 0;
                                 count_layer_next = count_layer+1;
-                                prev_lay_out = curr_lay_out;
                             end
+                            curr_lay_out[count_neuron] = curr_neuron_out; 
                         end
             done : begin
                     out_values = curr_lay_out;
@@ -60,12 +63,10 @@ module FSM #(parameter int_part=3, fract_part=5, N=8, M=3)(
     
     always_comb begin
         for(int i=0; i<N; i++) begin
-            for(int j=0; j<N; j++) begin
-                current_weight[i][j] = weight[count_neuron][i][j];
-            end            
+            current_weight[i] = weight[count_layer][count_neuron][i];              
         end
     end
     
-    layer inst(clk, count_neuron, prev_lay_out, current_weight, curr_lay_out);
+    single_neuron inst(prev_lay_out, current_weight, bias, curr_neuron_out);  
 
 endmodule
